5c03a43185ac1d5a62ab79b19cd460829f1e7865
gfcwfzkm <gfcwfzkm@protonmail.com>
2021-10-17 20:14:50 +0200

    added example code

--- example/example.c
+++ example/example.c
@@ -7,12 +7,16 @@
 #define SDA         	BIT(7)	// PB.7 is SDA
 #define I2C_SPEED		100000	// 100kHz I2C speed
 #define M24C16_ADDRESS	0xA0	// Address of the M24C16 I2C EEPROM (2kByte)
+// Should work with any of the M24Cxx series I2C EEPROMs tho!
 
 /* I2C Driver Interface */
 TWI_Master_t twiInstance;
 
 int main(void)
 {
+	// First byte represents the internal eeprom address, the rest the test data
+	uint8_t testData[9] = {0,1,2,3,4,5,6,7,8};
+
 	/* Enable clocks for peripherals */
 	rcu_periph_clock_enable(RCU_GPIOB);
 	rcu_periph_clock_enable(RCU_I2C0);
@@ -28,4 +32,42 @@ int main(void)
 
 	/* Now finally initialise the TWI library */
 	twim_init(&twiInstance, I2C0, I2C_SPEED);
+
+	/* Lets start the operation and wait for it to finish */
+	twim_write(&twiInstance, M24C16_ADDRESS, testData, 9);
+	while(twim_ready(&twiInstance) == TWIM_STATUS_BUSY);
+
+	/* Check if the operation performed without errors */
+	if (twim_result(&twiInstance) != TWIM_RES_OK)
+	{
+		/* An error occured! */
+		while(1);
+	}
+
+	/* Now lets read back the data. Since we have to state the internal address of 
+	 * the eeprom, we have to perform a write-read operation using the first testData 
+	 * entry again, followed by reading back 8 bytes from the eeprom */
+	twim_writeRead(&twiInstance, M24C16_ADDRESS, testData, 1, 8);
+	while(twim_ready(&twiInstance) == TWIM_STATUS_BUSY);
+
+	/* Check if the operation performed without errors */
+	if (twim_result(&twiInstance) != TWIM_RES_OK)
+	{
+		/* An error occured! */
+		while(1);
+	}
+
+	/* Low lets compare the read back data against the testData array. The first index is
+	 * not part of the data but part of the eeprom address */
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		if (testData[i+1] != twiInstance.readBuffer[i])
+		{
+			/* EEPROM Data not the same as what we've sent before */
+			while(1);
+		}
+	}
+
+	/* Test has been performed sucessfully! Loop forever */
+	while(1);
 }
